ffd406fdc4e945c0249972828f16d4a8dc906fb3
Yugam Aggarwal <yugamaggarwal007@gmail.com>
2024-04-28 12:29:07 +0530

    learnt about break and conitnue

---
+++ 07-break-and-continue.py
@@ -0,0 +1,7 @@
+'''
+In programming, the break and continue statements are used to alter the flow of loops:
+
+break exits the loop entirely
+continue skips the current iteration and proceeds to the next one
+
+'''
